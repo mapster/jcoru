c92598b4d63e9375416cd2a7cebfe6e997d19209
mapster <alexander@rosbach.no>
2015-04-06 01:39:38 +0200

    Added JUnitTestRunner, WIP. WIP CompilerResource.

--- src/test/resources/fixtures/UnitTest.java
+++ src/test/resources/fixtures/UnitTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -10,5 +11,10 @@ public class UnitTest {
     @Test
     public void test() {
         assertTrue(true);
+    }
+
+    @Test
+    public void failingTest() {
+        assertEquals("true", "false");
     }
 }
