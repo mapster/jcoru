37b00119d6a83488e4bef4aedf2d8e82b23a3982
mapster <alexander@rosbach.no>
2015-04-24 23:05:31 +0200

    WIP CompilerResource and CompilationReport.

--- src/test/java/no/rosbach/edu/compiler/fixtures/Fixtures.java
+++ src/test/java/no/rosbach/edu/compiler/fixtures/Fixtures.java
@@ -3,9 +3,9 @@ package no.rosbach.edu.compiler.fixtures;
 import no.rosbach.edu.compiler.JavaSourceString;
 import org.apache.commons.io.IOUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
 
 /**
  * Created by mapster on 05.04.15.
@@ -31,5 +31,20 @@ public enum Fixtures {
         } catch (IOException e) {
             throw new Error("Failed to read fixture java source.", e);
         }
+    }
+
+    public static JavaSourceString getFixtureInterfaceSource(Fixtures className) {
+        String fileName = className + ".java";
+        try (InputStream sourceStream = new FileInputStream(compactPath("src","test","java","no","rosbach","edu","compiler","fixtures", fileName))) {
+            return new JavaSourceString(fileName, IOUtils.toString(sourceStream));
+        } catch (FileNotFoundException e) {
+            throw new Error(String.format("Could not find interface source for fixture %s", className), e);
+        } catch (IOException e) {
+            throw new Error(String.format("Failed to read interface for fixture %s", className), e);
+        }
+    }
+
+    private static String compactPath(String... path) {
+        return StreamSupport.stream(Arrays.spliterator(path), false).reduce((s1, s2) -> s1 + File.separatorChar + s2).get();
     }
 }
