3dcbbd5a27127da212fa5150119cc13b868bf068
mapster <alexander@rosbach.no>
2015-05-10 14:45:49 +0200

    Removed old factory pattern.

--- src/main/java/no/rosbach/jcoru/factory/JavaCompilerFactory.java
+++ src/main/java/no/rosbach/jcoru/factory/JavaCompilerFactory.java
@@ -1,20 +1,14 @@
 package no.rosbach.jcoru.factory;
 
-import org.glassfish.hk2.api.Factory;
-
 import javax.enterprise.inject.Produces;
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 
-public class JavaCompilerFactory implements Factory<JavaCompiler> {
+public class JavaCompilerFactory {
 
   @Produces
   public JavaCompiler provide() {
     return ToolProvider.getSystemJavaCompiler();
   }
 
-  @Override
-  public void dispose(JavaCompiler compiler) {
-
-  }
 }
