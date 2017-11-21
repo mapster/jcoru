cd676ef8e84771eb8cdc183fee54dd156a86625a
Alexander Hoem Rosbach <alexander@rosbach.no>
2017-11-21 20:45:57 +0100

    Fix memory leak

--- src/main/java/no/rosbach/jcoru/provider/JavaCompilerProvider.java
+++ src/main/java/no/rosbach/jcoru/provider/JavaCompilerProvider.java
@@ -2,6 +2,7 @@ package no.rosbach.jcoru.provider;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import javax.tools.*;
 
@@ -10,11 +11,13 @@ public class JavaCompilerProvider {
   private JavaCompiler javaCompiler = ToolProvider.getSystemJavaCompiler();
 
   @Bean
+  @Scope("request")
   public JavaCompiler javaCompiler() {
     return javaCompiler;
   }
 
   @Bean
+  @Scope("request")
   public JavaFileManager systemFileManager() {
     return javaCompiler.getStandardFileManager(null, null, null);
   }
